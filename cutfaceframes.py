68f6aa00d8ed429e906f70a89aa8337e64e0e0b7
surrey2024 <b05902024@ntu.edu.tw>
2019-12-31 11:39:37 +0800

    small fix

--- cutfaceframes.py
+++ cutfaceframes.py
@@ -1,14 +1,17 @@
 import os, sys, cv2
+import shutil
 import numpy as np
 
 try:
     video_in, out_dir = str(sys.argv[1]), str(sys.argv[2])
 except IndexError:
     print('Format: python {} [video_in] [out_dir]'.format(sys.argv[0]))
+    sys.exit(1)
 
+# out_dir = out_dir.encode('unicode_escape')
 vid = cv2.VideoCapture(video_in)
 
-os.system('rm -fr {}'.format(out_dir))
+shutil.rmtree('{}'.format(out_dir), ignore_errors=True)
 os.mkdir(out_dir)
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
@@ -19,16 +22,22 @@ while(True):
     ret, img = vid.read()
     if ret:
         gray_frame = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        faces = sort(face_cascade.detectMultiScale(gray_frame, 1.3, 5), key=lambda x: -x[0])
+        faces = sorted(face_cascade.detectMultiScale(gray_frame, 1.3, 5), key=lambda x: -x[0])
         x, y, w, h = faces[0]
-        cv2.imwrite('{}/{:03d}.jpg'.format(out_dir, cnt), gray_frame[y:y+h,x:x+w])
+        
+        s = os.path.join(out_dir,'{:03d}.jpg'.format(cnt))
+        # print(s)
+        # cv2.imencode('.jpg', gray_frame[y:y+h,x:x+w])[1].tofile(s)
+        if not cv2.imwrite(s, gray_frame[y:y+h,x:x+w]):
+            raise Exception("Could not write image")
         cnt += 1
     else:
         break
 
+print(cnt)
 vid.release()
 
 start, end = cnt//5, cnt//5*4
-for i in range(start): os.system('rm -f {}/{:03d}.jpg'.format(out_dir, i))
-for i in range(end, cnt): os.system('rm -f {}/{:03d}.jpg'.format(out_dir, i))
-for i in range(start, end): os.system('mv {}/{:03d}.jpg {}/{:03d}.jpg'.format(out_dir, i, out_dir, i-start))
+for i in range(start): os.remove(os.path.join(out_dir,'{:03d}.jpg'.format(i)))
+for i in range(end, cnt): os.remove(os.path.join(out_dir,'{:03d}.jpg'.format(i)))
+for i in range(start, end): os.rename(os.path.join(out_dir,'{:03d}.jpg'.format(i)), os.path.join(out_dir,'{:03d}.jpg'.format(i-start)))
