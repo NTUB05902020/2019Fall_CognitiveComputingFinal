783f8efa0ca0be41551777caab9a5c036f2e07be
al7771998 <b05902018@ntu.edu.tw>
2019-12-31 22:09:32 +0800

    AAAAA

--- delete_data.py
+++ delete_data.py
@@ -7,10 +7,11 @@ _, dirs, _ = list(os.walk(dest))[0]
 for AU_dir in dirs:
 	_, indice, _ = list(os.walk(dest + AU_dir + '/'))[0]
 	for index in indice:
-		os.system('mv ' + dest + AU_dir + '/' + index + '/aligned/' + 'features_lpq.npy '\
-			+ dest + AU_dir + '/' + index + '/features_lpq.npy')
+		os.system('mv ' + dest + AU_dir + '/' + index + '/aligned/' + 'features_lbp.npy '\
+			+ dest + AU_dir + '/' + index + '/features_lbp.npy')
 		files = os.listdir(dest + AU_dir + '/' + index)
 		for file in files:
 			if file == "aligned":
 				os.system('rm -r {}'.format(dest + AU_dir + '/' + index + '/' + file))
-		
+			elif file[-4:] != '.npy':
+				os.system('rm {}'.format(dest + AU_dir + '/' + index + '/' + file))
